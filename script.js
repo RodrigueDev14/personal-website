7c29c03207eae0301f19709c357ae9355e7a2873
Rodrigue Houelete Wanonkou <houeleterodrigue@gmail.com>
2025-06-30 16:12:09 +0000

    a

--- script.js
+++ script.js
@@ -96,7 +96,7 @@ const projects = [
       "Divi Solutions Tech est une entreprise basée à Montréal, Québec, qui se dédie à aider les individus, et particulièrement les nouveaux arrivants francophones et les résidents du Québec, à maîtriser les outils et technologies numériques.", // Plateforme pour analyser les tendances des médias sociaux et l\'analyse des sentiments
     technologies: ["Wordpress", "Elementor"], // Technologies utilisées : Python, NLTK, FastAPI, React, PostgreSQL
     github: "#",
-    demo: "divisolutionstech.ca",
+    demo: "https://www.divisolutionstech.ca",
     image:
       "images/diviSolution.png", // Chemin de l'image du projet sadaci
   },
